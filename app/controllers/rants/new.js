c8e47af08025d6d5b48b394d6a51255e4d9bda21
Dmidd87 <dmidd87@gmail.com>
2015-03-28 12:56:47 -0600

    Added rants index, got update, delete, and cancel actions working

---
+++ app/controllers/rants/new.js
@@ -0,0 +1,29 @@
+import Ember from 'ember';
+
+export default Ember.ObjectController.extend({
+
+  actions: {
+    saveRant: function() {
+
+      var title = this.get('rantTitle');
+      var body = this.get('rantBody');
+      var userid = this.get('session.user_id');
+      var self = this;
+      console.log(this.store.find('user', userid));
+      console.log(this.store.find('user'));
+
+      this.store.find('user', userid).then(function(userobject) {
+        var rant = self.store.createRecord('rant', {user: userobject, body: body, title: title });
+        self.set('rantTitle', '');
+        self.set('rantBody', '');
+        rant.save().then(function (){
+          self.transitionToRoute('rants');
+        });
+      });
+    },
+
+    cancelRant: function() {
+      this.transitionToRoute('rants');
+    }
+  }
+});
