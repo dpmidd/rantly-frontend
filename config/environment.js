41a626d913ba5ee36e11c3b4ab0ecd8cad88d53a
Dmidd87 <dmidd87@gmail.com>
2015-03-29 15:06:59 -0600

    Added env info

--- config/environment.js
+++ config/environment.js
@@ -18,7 +18,7 @@ module.exports = function(environment) {
       'default-src': "'none'",
       'script-src': "'self'",
       'font-src': "'self' http://fonts.gstatic.com http://maxcdn.bootstrapcdn.com/",
-      'connect-src': "'self' localhost:3000",
+      'connect-src': "'self' " + process.env.ADAPTER_URL,
       'img-src': "'self' http://www.gravatar.com/",
       'style-src': "'self' 'unsafe-inline' http://fonts.googleapis.com http://maxcdn.bootstrapcdn.com/",
       'media-src': "'self'"
@@ -36,6 +36,7 @@ module.exports = function(environment) {
   };
 
   ENV['simple-auth-devise'] = {
+    serverTokenEndpoint: process.env.ADAPTER_URL,
     crossOriginWhitelist: ['*'],
     identificationAttributeName: 'email'
   };
